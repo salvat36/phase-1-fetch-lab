02125636e031e00ba0dc00d656318b3380191c66
Ryan <salvat36@gmail.com>
2023-04-14 04:57:39 -0500

    finally figured out what they meant by returning fetch

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
+  
 
+  // To pass the tests, don't forget to return your fetch!
+
+  
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
